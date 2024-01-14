ce333739bdfec61ae108a708620bfe1892342f9b
Joo-Byungho <kjcoco13@gmail.com>
2024-01-14 16:33:33 +0900

    Refactor: Change component letter case Camel case

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   },
   docs: {
     autodocs: 'tag',
