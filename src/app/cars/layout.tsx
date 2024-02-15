782bee8f99db03baa7128e774fc6272786b05cba
Joo-Byungho <kjcoco13@gmail.com>
2024-02-15 17:32:01 +0900

    Feat: Implement metadata to cars layout

--- src/app/cars/layout.tsx
+++ src/app/cars/layout.tsx
@@ -1,6 +1,12 @@
 import { ReactNode } from 'react';
 
+import { Metadata } from 'next';
+
 import 'server-only';
+
+export const metadata: Metadata = {
+  title: 'Car-List',
+};
 
 export default function CarsLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
