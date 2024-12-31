b961df17bacf9b4fe40bbde97a280f5186dc0c4b
Joo-Byungho <kjcoco13@gmail.com>
2024-12-31 17:20:13 +0900

    Fix: Handle fetch failed error occurred when during github actions
    - ref: https://github.com/vercel/next.js/issues/44062#top

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -2,6 +2,8 @@ import 'server-only';
 
 import Cars from '~/components/Cars';
 
+export const dynamic = 'force-dynamic';
+
 export default async function CarsPage(...props: any) {
   return <Cars latency={4000} />;
 }
